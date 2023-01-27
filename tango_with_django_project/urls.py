cd53a41336d56d11fb8ff610713eaa16c76175c2
Firstname Surname <2651113m@student.gla.ac.uk>
2023-01-27 15:53:39 +0000

    ch 3.6

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -20,5 +20,7 @@ from rango import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('rango/', include('rango.urls')),
+    # The above maps any URLs starting with rango/ to be handled by rango.
     path('admin/', admin.site.urls),
 ]
