15c638d189daacd67e7b4dfd2eacb5ec257724e5
Robert Molloy <2651113m@student.gla.ac.uk>
2023-01-28 16:25:58 +0000

    ch 4.4

--- rango/views.py
+++ rango/views.py
@@ -7,4 +7,4 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
+    return render(request, 'rango/about.html')
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -25,5 +25,4 @@ urlpatterns = [
     path('rango/', include('rango.urls')),
     # The above maps any URLs starting with rango/ to be handled by rango.
     path('admin/', admin.site.urls),
-    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
