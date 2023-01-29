a7ebd8242f529e3f3edd9e7959617935825eb8c9
Robert Molloy <2651113m@student.gla.ac.uk>
2023-01-29 14:02:21 +0000

    ch 5.8

--- rango/models.py
+++ rango/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
 
     class Meta:
         verbose_name_plural = 'Categories'
@@ -17,4 +19,6 @@ class Page(models.Model):
     
     def __str__(self):
         return self.title
+
+
 
