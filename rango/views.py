ea20e9df918fa112bd424c3807fd73164c7297d4
Robert Molloy <2651113m@student.gla.ac.uk>
2023-02-02 17:09:16 +0000

    ch 7.2, form models, and clean() for page form

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
-from rango.models import Category
-from rango.models import Page
+from rango.models import Category,Page
+from rango.forms import CategoryForm
+
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -32,4 +33,18 @@ def show_category(request, category_name_slug):
         context_dict['pages'] = None
 
     return render(request, 'rango/category.html', context=context_dict  )
+
+def add_category(request):
+    form = CategoryForm()
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return redirect('/rango/')
+        else:
+            print(form.errors)
+
+    return render(request, 'rango/add_category.html', {'form': form})
 
