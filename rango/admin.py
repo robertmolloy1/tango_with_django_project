2934030a5990488566098ca5a347c2d78e0c038a
Robert Molloy <2651113m@student.gla.ac.uk>
2023-02-08 22:33:23 +0000

    ch 9.5, added user model and additional fields

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from rango.models import Category, Page
+from rango.models import Category, Page, UserProfile
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('name',)}
@@ -11,3 +11,4 @@ class PageAdmin(admin.ModelAdmin):
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page,PageAdmin)
+admin.site.register(UserProfile)
