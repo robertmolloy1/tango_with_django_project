0fb54f3a834ae19729a4eb6bae2ecdcc7857f732
Robert Molloy <2651113m@student.gla.ac.uk>
2023-02-09 22:06:55 +0000

    ch 10.6, changed to server-side cookies

--- rango/views.py
+++ rango/views.py
@@ -8,19 +8,25 @@ from rango.forms import CategoryForm,PageForm,UserForm,UserProfileForm
 from datetime import datetime
 
 
-def visitor_cookie_handler(request, response):
-    visits = int(request.COOKIES.get('visits', '1'))
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
 
-    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
     last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
 
     if (datetime.now() - last_visit_time).days > 0:
         visits = visits + 1
-        response.set_cookie('last_visit', str(datetime.now()))
+        request.session['last_visit'] = str(datetime.now())
     else:
-        response.set_cookie('last_visit', last_visit_cookie)
+        request.session['last_visit'] = last_visit_cookie
 
-    response.set_cookie('visits', visits)
+    request.session['visits'] = visits
     
 
 def index(request):
@@ -31,12 +37,11 @@ def index(request):
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
-    context_dict['visits'] = int(request.COOKIES.get('visits', '1'))
 
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    
     response = render(request, 'rango/index.html', context=context_dict)
-
-    visitor_cookie_handler(request, response)
-
     return response
 
 def about(request):
