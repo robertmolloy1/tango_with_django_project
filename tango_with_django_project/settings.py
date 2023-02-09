0d9be49f89eb076854b0ff36e6e550b820493154
Robert Molloy <2651113m@student.gla.ac.uk>
2023-02-09 12:02:06 +0000

    ch 9.8, login required functionality implemented

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -17,6 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+LOGIN_URL = 'rango:login'
 
 
 # Quick-start development settings - unsuitable for production
